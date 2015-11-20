98d00a9a0596e8366c7a621eb39fdfe5f9e2d16c
Vijay Jujjuri <vijay.jujjuri@kore.com>
2015-11-20 18:12:20 +0530

    routing placed on before hand

--- app.js
+++ app.js
@@ -26,12 +26,11 @@ fs.readdirSync(join(__dirname, 'app/models')).forEach(function (file) {
 // passport config
 require('./config/passport')(passport);
 
+// routes
+require('./config/routing')(app, passport);
 
 //application settings
 require('./config/express')(app, passport);
-
-// routes
-require('./config/routing')(app, passport);
 
 app.listen(port);
 console.log('Express app started on port ' + port);
