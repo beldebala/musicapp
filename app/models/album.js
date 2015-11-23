ba19857b1665480ebfa522d225fdda7ab27e9ffe
beldebala <belde.bala16@gmail.com>
2015-11-23 16:34:06 -0600

    moved tracks in to  albums model

--- app/models/album.js
+++ app/models/album.js
@@ -6,6 +6,11 @@ var Schema = mongoose.Schema;
 var albumSchema = new Schema({
 	user_id : {type: String, default: ''},
 	title : {type: String, default: ''},
+	tracks : [{
+	title : {type: String,default: ''},
+	description : {type: String,default: ''},
+	track_filename : {type: String,default: ''}
+}],
 	img_filename : {type: String, default: 'default_album.jpg'}
 });
 
